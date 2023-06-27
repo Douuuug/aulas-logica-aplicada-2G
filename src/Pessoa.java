e63ae1be164fc4bb8b52b5e6d6f76b2718075e8d
Douglas <126492657+Douuuug@users.noreply.github.com>
2023-06-27 14:55:17 -0300

    Update Pessoa.java

--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,4 +1,13 @@
 public class Pessoa {
-    //Atributo
-     String nomes;
+    // Atributos da classe
+    String nome;
+    String sobrenome;
+    // Métodos da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+    public void falar(String volume){
+        System.out.println("falei " + volume);
+    }
 }
+
