c85637a1b14d1366f5296feb7c257c92a7a1be01
Douglas <126492657+Douuuug@users.noreply.github.com>
2023-06-27 14:54:47 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-       // Aula introdutória de Orientação a Objetos
-
+        // Aula introdutória de Orientação à Objetos
+        // Criar objetos
+        Pessoa adao = new Pessoa();
+        // Declaração de objeto
+        Pessoa qualquer;
+        // Instanciação do objeto
+        qualquer = new Pessoa();
+        Pessoa adulta = new Pessoa();
+        // Definir forma do objeto
+        qualquer.nome = "Rita";
+        qualquer.sobrenome = "Lee";
+        // Definir comportamento do objeto
+        qualquer.falar();
+        adao.falar("baixo");
+        
 
     }
 }
